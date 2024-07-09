c1b1c189a30c470e7013592979592c335540e20a
ishikanimmagadda <ishikan84@gmail.com>
2024-07-08 20:25:46 -0500

    fixed code, run successfully

--- LinkedList.c
+++ LinkedList.c
@@ -1,78 +1,76 @@
 #include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 
 // define the linked list node 
 typedef struct node { 
     void* data; 
     struct node* next; 
-}node;
+} node;
 
 typedef struct linkedlist {
     node* head; 
     size_t size; 
-    //void (*copy_data)(void *dest, void *src); //copying data from src location to destination
-    //void (*free_data)(void *data); // freeing space in memory 
-}linkedlist;
+} linkedlist;
 
-void initialize_linked_list(linkedlist* list, size_t size){
-       list->head = (node*)malloc(sizeof(node));
-       list->size = 0; 
-       //list->copy_data = copy_data; 
-       //list->free_data = free_data;  
+void initialize_linked_list(linkedlist* list){
+    list->head = NULL;
+    list->size = 0; 
 }
 
-void addElement(linkedlist* list, void* data){ 
-    node* newnode = (node*)malloc(sizeof(node)); // node you want to add 
-    node* cur = list->head; 
-    newnode->data = malloc(sizeof(data)); // making space for the data in the node 
-    while(cur->next != NULL){ 
-        cur = cur->next; 
-    } // until list->head is the last element 
-
-    cur->next = newnode; // giving space for the new element 
-    newnode->data = data; //populating new node with data 
-    cur->next->next = NULL; //resetting last element to NULL 
-}
-
-void* removeElement (linkedlist* list, size_t index){ 
-   int i = 0; 
-   node* cur = list->head; 
-   node* temp = NULL; 
-   node* next = list->head->next; 
-
-   if (index == 0) { 
-        if(list->head == NULL){
-           printf ("empty");
-           return NULL;
-        }
-        void* tempdata = list->head->data;
-        temp = list->head; 
-        free(temp);
-        list->head = next; 
-        return tempdata; 
-   }
-
-   for(i=0; i < index-1; i++){
-    if (cur->next == NULL){ //index out of bounds 
-        return "out of bounds";
+void addElement(linkedlist* list, void* value){ 
+    node* newnode = (node*)malloc(sizeof(node));
+    if (newnode == NULL) {
+        printf("Memory allocation failed");
+        return; 
     }
-    cur = cur->next; 
-   }
 
-   if (cur->next == NULL){
-        return "empty"; 
-   }
+    newnode->data = value; 
+    newnode->next = NULL; 
 
-   temp = cur->next; 
-   cur->next = temp->next;
-   free(temp); 
+    if (list->head == NULL) {
+        list->head = newnode;
+    } else {
+        node* cur = list->head;
+        while (cur->next != NULL){ 
+            cur = cur->next; 
+        }
+        cur->next = newnode; // giving space for the new element 
+        cur->next->next = NULL;
+    }
+    list->size++;
+}
 
-   return "removed";
+void* removeElement(linkedlist* list, size_t index){ 
+    if (index >= list->size) {
+        printf("Index out of bounds");
+        return NULL;
+    }
+
+    node* temp = NULL; 
+    void* tempdata = NULL; 
+
+    if (index == 0) { //removing first element 
+        temp = list->head;
+        tempdata = temp->data;
+        list->head = list->head->next;
+        free(temp);
+        
+    } else {
+        node* cur = list->head; 
+        for(int i=0; i < index-1; i++){ // all the way until one before the index node 
+            cur = cur->next; // cur is now the node you want to remove 
+        }
+        temp = cur->next; 
+        tempdata = temp->data; 
+        cur->next = temp->next; 
+        free(temp); 
+    }
+    list->size--;
+    return tempdata; 
 }
 
 void* accessElement (linkedlist* list, size_t index){
-    node* cur = list->head; 
-    int i = 0; 
+    node* cur = list->head;  
     if (index >= list->size){ 
         return NULL;
     }
@@ -81,6 +79,57 @@ void* accessElement (linkedlist* list, size_t index){
         cur = cur->next; 
     }
 
-    return cur; 
+    return cur->data; 
 }
 
+void printList(linkedlist* list) {
+    node* cur = list->head;
+    while (cur != NULL) {
+        printf("%d -> ", *(int*)(cur->data)); // Assuming data is integer 
+        cur = cur->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    linkedlist list;
+    initialize_linked_list(&list);
+
+    // Adding elements to the linked list
+    int a = 5;
+    int b = 10;
+    int c = 15;
+    int d = 20;
+
+    addElement(&list, &a);
+    addElement(&list, &b);
+    addElement(&list, &c);
+    addElement(&list, &d);
+
+    // Printing the initial list
+    printf("Initial List: ");
+    printList(&list);
+
+    // Removing an element at index 2
+    void* removedData = removeElement(&list, 2);
+    if (removedData != NULL) {
+        printf("Removed element: %d\n", *(int*)removedData);
+    }
+
+    printf("List after removing element at index 2: ");
+    printList(&list);
+
+    // Accessing an element at index 1
+    void* accessedData = accessElement(&list, 1);
+    if (accessedData != NULL) {
+        printf("Accessed element at index 1: %d\n", *(int*)accessedData);
+    }
+
+    // Freeing memory
+    while (list.head != NULL) {
+        void* data = removeElement(&list, 0);
+        if (data != NULL) {
+        }
+    }
+    return 0;
+}
